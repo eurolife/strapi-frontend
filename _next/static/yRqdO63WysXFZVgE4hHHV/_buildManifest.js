6cb6ec51858be56684831415ab9f483105fb3b1c
eurolife <eurolife@users.noreply.github.com>
2021-09-24 10:45:33 +0000

    deploy: 19487660ec62b5d88173a94bd69507a8be6cecf4

---
+++ _next/static/yRqdO63WysXFZVgE4hHHV/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-aca6d7f1e4394664e906.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
