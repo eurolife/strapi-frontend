f3979f9ba1cbe22ec419742f99be72a4d9f2af66
Paula Hightower <eurolife@gmail.com>
2021-09-28 14:47:30 +0100

    updated api

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -4,6 +4,7 @@ export function getStrapiURL(path = '') {
   // const url = 'ec2-3-8-148-62.eu-west-2.compute.amazonaws.com';
   // const url = 'localhost:1337'
   return `${CMS_URL}${path}`;
+  // return `http://localhost:1337${path}`;
 }
 
 // Helper to make GET requests to Strapi
@@ -13,3 +14,19 @@ export async function fetchAPI(path) {
   const data = await response.json();
   return data;
 }
+
+export async function getPageData(slug, preview = false) {
+  // Find the pages that match this slug
+  console.log(preview)
+  const pagesData = await fetchAPI(
+    `/pages?slug=${slug}&status=published${preview ? "&status=draft" : ''}`
+  );
+ 
+  // Make sure we found something, otherwise return null
+  if (pagesData == null || pagesData.length === 0) {
+    return null;
+  }
+ 
+  // Return the first item since there should only be one result per slug
+  return pagesData[0];
+ }
